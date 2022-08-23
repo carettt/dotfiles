a0da396a5d7c6970b05751d703ab3f26abe00c47
Luna Andrade <doctor.diebo@gmail.com>
2022-08-23 13:50:43 +0100

    Added zsh shell and updated install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-sudo pacman -Syy alacritty i3-gaps i3lock neofetch nvim polybar feh paru
+sudo pacman -Syy alacritty i3-gaps i3lock neofetch neovim polybar feh zsh lxappearance paru
 paru -S themix-full-git picom-ibhagwan-git
-cp -r alacritty i3 neofetch nvim oomox picom polybar ~/.config
-cp -r .themes .bg1920.jpg .bg2560.jpg ~/
+chsh $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo cp -r ./alacritty ./i3 ./neofetch ./nvim ./oomox ./picom ./polybar ~/.config/
+cp -r .themes .bg1920.jpg .bg2560.jpg .zshrc ~/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
