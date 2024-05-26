66c94f16277927627aae7333dac58448aac4dfbe
caret_ <dandradepac@outlook.com>
2024-05-26 10:56:43 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "NixOS Config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.patch = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/default/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
