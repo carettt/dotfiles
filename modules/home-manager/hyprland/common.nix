f1e5a1c44a39e91eb0305630dba8693afe8fd8d4
caret_ <dandradepac@outlook.com>
2024-06-01 01:18:07 +0100

    Add common hyprland config and edit home-manager defualt.nix to exclude theme-*.nix

--- modules/home-manager/hyprland/common.nix
+++ modules/home-manager/hyprland/common.nix
@@ -15,6 +15,44 @@ let cfg = config.hyprland; in {
     wayland.windowManager.hyprland = {
       enable = true;
       package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+
+      settings = {
+        # Monitors
+        monitor= [
+          ",preferred,auto,auto"
+          "Unknown-1,disable" # Disable ghost monitor (fuck NVIDIA)
+        ];
+
+        # Program Shortcuts
+        "$terminal" = "alacritty";
+        "$menu" = "";
+
+        # Autostart
+        exec-once = [
+          
+        ];
+
+        # Environment Variables
+        env = [
+          "XCURSOR_SIZE,24"
+          "HYPRCURSOR_SIZE,24"
+        ];
+
+        # Aesthetic
+
+        ## Tiling
+        general = {
+          gaps_in = 5;
+          gaps_out = 20;
+
+          border_size = 2;
+          resize_on_border = false;
+
+          allow_tearing = false;
+
+          layout = "dwindle";
+        };
+      };
     };
   };
 }
