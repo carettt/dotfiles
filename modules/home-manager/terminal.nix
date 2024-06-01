d163ac13de85d12299819b3bf8136e18e8734c12
caret_ <dandradepac@outlook.com>
2024-06-01 14:09:13 +0100

    Add default Alacritty config

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -3,9 +3,36 @@
 let cfg = config.terminal; in {
   options = {
     terminal.enable = lib.mkEnableOption "terminal packages";
+
+    terminal.alacrittyTheme = lib.mkOption {
+      type = lib.types.path;
+      description = "Path to alacritty theme named theme-[NAME].nix (overwrites default)";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    programs.alacritty.enable = true;
+    home.packages = [
+      (pkgs.nerdfonts.override { fonts = ["Hack" ]; })
+    ];
+
+    programs.alacritty = {
+      enable = true;
+
+      settings = {
+        window = {
+          padding = { x = 5; y = 5; };
+          decorations = "None";
+        };
+
+        font = {
+          size = 12.0;
+          normal.family = "Hack";
+          bold.family = "Hack";
+          italic.family = "Hack";
+        };
+
+        cursor.style = { shape = "Beam"; blinking = "On"; };
+      };
+    };
   };
 }
