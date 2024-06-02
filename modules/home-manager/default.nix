459ef5382f36ed6695bddac9620ef396f150c017
caret_ <dandradepac@outlook.com>
2024-06-02 19:20:47 +0100

    Some refactoring

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,5 +1,5 @@
 { lib, ... }:
 
 {
-  imports = lib.fileset.toList (lib.fileset.fileFilter (file: file.name != "default.nix" && file.name != "template.nix" && ! lib.strings.hasPrefix "theme-" file.name && file.hasExt "nix") ./.);
+  imports = lib.fileset.toList (lib.fileset.fileFilter (file: file.name != "default.nix" && file.name != "template.nix" && file.hasExt "nix") ./.);
 }
