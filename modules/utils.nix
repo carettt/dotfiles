2bd803497edebdc39075350ab0e15a1ec15e5ab6
caret_ <dandradepac@outlook.com>
2024-06-02 20:18:31 +0100

    Add credit

--- modules/utils.nix
+++ modules/utils.nix
@@ -1,7 +1,8 @@
 { lib }:
 
 let
-  # Define the recursiveMerge function
+  # Credit to: srghma (https://stackoverflow.com/questions/54504685/nix-function-to-merge-attributes-records-recursively-and-concatenate-arrays)
+  # Merges list of records, concatenates arrays, if two values can't be merged - the latter is preferred
   recursiveMerge = attrList:
     let f = attrPath:
       lib.zipAttrsWith (n: values:
