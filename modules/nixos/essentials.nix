67b8f142a215724dc3e91b0296b6f5fd5d5100e9
caret_ <dandradepac@outlook.com>
2024-06-04 15:12:44 +0100

    Fix thefuck by disabling fix_file rule

--- modules/nixos/essentials.nix
+++ modules/nixos/essentials.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }:
+{ lib, builtins, config, pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [ 
@@ -8,4 +8,18 @@
     ripgrep
     thefuck
   ];
+
+  home-manager.users."caret" = {
+    xdg.configFile = {
+      "thefuck/settings.py".text = ''
+        exclude_rules = [ "fix_file" ]
+      '';
+    };
+    
+    programs.thefuck = {
+      enable = true;
+      enableNushellIntegration = true;
+      package = pkgs.thefuck;
+    };
+  };
 }
