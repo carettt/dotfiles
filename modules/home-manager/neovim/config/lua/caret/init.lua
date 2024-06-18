a171612b44bfde7f5fab89e8dc9763dd05b7e161
caret_ <dandradepac@outlook.com>
2024-06-18 19:16:06 +0100

    Change telescope keybinds to 'search' mnemonic instead of 'find' mnemonic

--- modules/home-manager/neovim/config/lua/caret/init.lua
+++ modules/home-manager/neovim/config/lua/caret/init.lua
@@ -21,9 +21,9 @@ require('rose-pine').setup({ variant = 'moon' })
 
 -- telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>sf', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fs', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ps', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ss', builtin.live_grep, {})
 
 -- harpoon
 local harpoon = require('harpoon')
