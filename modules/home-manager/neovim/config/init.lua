6b5baf06d5fcf64a8489343b83ebcda64751715a
caret_ <dandradepac@outlook.com>
2024-06-23 20:12:48 +0100

    Bugfix: revert cursor to beam when leaving neovim

--- modules/home-manager/neovim/config/init.lua
+++ modules/home-manager/neovim/config/init.lua
@@ -1,1 +1,8 @@
 require("caret")
+
+-- auto commands
+vim.api.nvim_create_autocmd("ExitPre", {
+  group = vim.api.nvim_create_augroup("Exit", { clear = true }),
+  command = "set guicursor=a:ver90",
+  desc = "Set cursor back to beam when leaving Neovim."
+})
