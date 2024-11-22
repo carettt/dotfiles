3b889b40a3f1a256272670a2681fbe6b01103830
caret_ <dandradepac@outlook.com>
2024-11-22 15:22:41 +0000

    add openvpn

--- modules/nixos/net.nix
+++ modules/nixos/net.nix
@@ -11,11 +11,9 @@ let cfg = config.net; in {
         description = "Whether to enable NMAP network scanner";
       };
 
-      wireshark.enable = lib.mkOption {
-        default = false;
-        type = lib.types.bool;
-        description = "Whether to enable WireShark packet sniffer";
-      };
+      wireshark.enable = lib.mkEnableOption "WireShark packet sniffer";
+
+      openvpn.enable = lib.mkEnableOption "OpenVPN protocol";
     };
   };
 
@@ -23,7 +21,8 @@ let cfg = config.net; in {
     (lib.mkIf cfg.enable {
       environment.systemPackages =
         lib.lists.optionals cfg.nmap.enable [ pkgs.nmap ] ++
-        lib.lists.optionals cfg.wireshark.enable [ pkgs.wireshark ];
+        lib.lists.optionals cfg.wireshark.enable [ pkgs.wireshark ] ++
+        lib.lists.optionals cfg.openvpn.enable [ pkgs.openvpn ];
     })
     
     (lib.mkIf (cfg.enable && cfg.wireshark.enable) {
