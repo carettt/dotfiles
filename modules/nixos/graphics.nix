a51e91fe861d3bbf7220a624ef85ce56f900486a
caret_ <dandradepac@outlook.com>
2025-04-07 17:51:55 +0100

    hotfix: force intel driver to load before nvidia driver at boot

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -10,28 +10,30 @@ let cfg = config.graphics; in {
 
   config = lib.mkMerge [
     (lib.mkIf cfg.nvidia.enable {
-     services.xserver.videoDrivers = [ "nvidia" ];
-     hardware.nvidia = {
-       modesetting.enable = true;
-       open = false;
-       nvidiaSettings = true;
-       package = config.boot.kernelPackages.nvidiaPackages.stable;
+      boot.initrd.kernelModules = [ "i915" ];
 
-       powerManagement = {
-         enable = true;
-       };
-     };
+      services.xserver.videoDrivers = [ "nvidia" ];
+      hardware.nvidia = {
+        modesetting.enable = true;
+        open = false;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+        powerManagement = {
+          enable = true;
+        };
+      };
     })
 
     (lib.mkIf cfg.enable {
-       hardware.graphics.enable = true;
+      hardware.graphics.enable = true;
 
-       console = {
-         earlySetup = true;
-         font = "${pkgs.terminus_font}/share/consolefonts/ter-i16n.psf.gz";
-         packages = [ pkgs.terminus_font ];
-         keyMap = "us";
-       };
+      console = {
+        earlySetup = true;
+        font = "${pkgs.terminus_font}/share/consolefonts/ter-i16n.psf.gz";
+        packages = [ pkgs.terminus_font ];
+        keyMap = "us";
+      };
     })
   ];
 }
